5038408e50754a34c34d8e79bf1e33f4085f9399
nati2323 <nati2.man.3@gmail.com>
2022-05-30 18:44:51 +0300

    Add index.js file, creat a collection

---
+++ script.js
@@ -0,0 +1,24 @@
+let bookCollection = [
+    {
+      "Title": "Biology",
+      "Author": "Ivan"
+
+    },
+    {
+        "Title": "Computer",
+        "Author": "massa"
+    },
+    {
+        "Title": "Math",
+        "Author": "Natnael"
+    },
+    {
+        "Title": "Art",
+        "Author": "John"
+    },
+    {
+        "Title": "Physics",
+        "Author": "Robert"
+    },
+    
+  ]
