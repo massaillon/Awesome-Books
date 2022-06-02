48fba9a915092e891a1093c514c8f242c058a7d6
nati2323 <nati2.man.3@gmail.com>
2022-06-02 15:11:42 +0300

    Update script.js file

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ const addButton = document.getElementById('Add');
 // Create a variable for holding the local data
 let books;
 // Check if the Local storarege is empty
-if (JSON.parse(localStorage.getItem('Book-library')) !== undefined) {
+if (JSON.parse(localStorage.getItem('Book-library')) ) {
   books = JSON.parse(localStorage.getItem('Book-library'));
 } else {
-  books = [];
+  books = [
+    { 
+      ID:1, 
+      Title: "400 Days", 
+      Author: "Chetan Bhagat",
+
+    ID:2, 
+    Title: "The Ickabog", 
+    Author: "J.K Rowling",
+
+    ID:3, 
+    Title: "The Christmas Pig",
+    Author: "J.K Rowling",
+    },
+  ];
 }
 
 // Create object that will get its value from the form input
@@ -17,14 +31,22 @@ if (JSON.parse(localStorage.getItem('Book-library')) !== undefined) {
 // Add a book to the local storage
 // Create a function for adding
 function addBook() {
-  const bookCollection = {
+    const bookCollection = {
     ID: Math.floor(Math.random() * 1000000),
     Title: title.value,
     Author: author.value,
   };
   books.push(bookCollection);
+   BookList.innerHTML = `${BookList.innerHTML}<div class="Book-Generate">
+      <p>${bookCollection.Title}</p>
+      <p>${bookCollection.Author}</p>
+      <button id=${bookCollection.ID} onclick ='removeBook(${bookCollection.ID})' type = "button" >Remove</button>
+      <hr>
+      </div>`;
+ 
   // Store the book to the local Storage
   localStorage.setItem('Book-library', JSON.stringify(books));
+  
 }
 // Add an event listener  for the add action to be performed
 addButton.addEventListener('click', addBook);
@@ -32,13 +54,18 @@ addButton.addEventListener('click', addBook);
 // Implement the Booklist section that has to be generated
 // Reference the elements
 const BookList = document.querySelector('.Book-library');
-// loop through the Array to find out the number of books object present
-
+function removeBook(bookId){
+  const lib = document.getElementById(bookId);
+    //lib.remove();
+    books = books.filter((book) => book.ID !== Number(bookId));
+    lib.parentElement.outerHTML='';
+    localStorage.setItem('Book-library', JSON.stringify(books));
+}
 books.forEach((element) => {
   BookList.innerHTML = `${BookList.innerHTML}<div class="Book-Generate">
     <p>${element.Title}</p>
     <p>${element.Author}</p>
-    <button onclick ='removeBook(${element.ID})' type = "button" >Remove</button>
+    <button id=${element.ID} onclick ='removeBook(${element.ID})' type = "button" >Remove</button>
     <hr>
     </div>`;
 });
